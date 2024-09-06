1ad3dc392479f038400582c4216bd149cba20769
iyeahh <yboravender@gmail.com>
2024-09-06 14:08:39 +0900

    Initial Commit

---
+++ CheckCoin/CheckCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckCoinApp.swift
+//  CheckCoin
+//
+//  Created by Bora Yang on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
